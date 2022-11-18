c2cba87f10f1bed54d7261c7eaff9d40d653512d
Alexander Romanov <alexander.romanov.271@gmail.com>
2022-11-18 14:08:43 +0300

    Commit #3

--- routes/greeting.py
+++ routes/greeting.py
@@ -1,8 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
+
+template = Jinja2Templates(directory='templates')
 greeting_router = APIRouter(prefix='/greeting')
 
 
 @greeting_router.get('')
-def print_message(name, message): 
-    return f'{name}! {message}'
+
+def print_message(request: Request, name, message): 
+    return template.TemplateResponse('index.html', {"request": request, "name": name, "message": message})
